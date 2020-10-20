8ad6d7fdded03761a8b4227e5556f6926844d052
Robert Blake <blake14@llnl.gov>
2020-10-20 14:14:35 -0700

    Adding new modes to wcs so it will compile.

--- var/spack/repos/builtin/packages/wcs/package.py
+++ var/spack/repos/builtin/packages/wcs/package.py
@@ -16,10 +16,17 @@ class Wcs(CMakePackage):
     version('master', branch='master')
     version('develop', branch='devel')
 
+    variant("des", default='none', 
+            description='Discrete event simulation engine to use',
+            values=('ross','none'), multi=False)
+    variant("exprtk", default=True, description="Use exprtk for inputs")
+
     depends_on('boost+graph+filesystem+regex+system')
     depends_on('sbml@5.18.0:+cpp')
     depends_on('cmake@3.12:', type='build')
     depends_on('cereal', type='build')
+    depends_on('ross', when="des=ross")
+    depends_on('mpi', when="des=ross")
 
     def cmake_args(self):
         spec = self.spec
@@ -28,6 +35,9 @@ class Wcs(CMakePackage):
             "-DCEREAL_ROOT:PATH=" + spec['cereal'].prefix,
             "-DSBML_ROOT:PATH=" + spec['sbml'].prefix,
             "-DWCS_WITH_SBML:BOOL=ON",
-            "-DWCS_WITH_EXPRTK:BOOL=ON",
         ]
+        if "+exprtk" in spec:
+            args.append("-DWCS_WITH_EXPRTK:BOOL=ON")
+        if "des=ross" in spec:
+            args.append("-DWCS_WITH_ROSS:BOOL=ON")
         return args
